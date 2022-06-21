eb8b856002a42e81afe60e7a95fccaf2552ffbc1
Ленар Хабибуллин <leonruscraft@gmail.com>
2022-06-21 13:09:48 +0500

    Add project files.

---
+++ PatternObserver/ConcreteSubject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternObserver
+{
+    class ConcreteSubject : Subject
+    {
+        private string _subjectState;
+
+        public string SubjectState
+        {
+            get { return _subjectState; }
+            set { _subjectState = value; }
+        }
+    }
+}
---
+++ PatternObserver/Program.cs
@@ -0,0 +1,12 @@
+﻿using PatternObserver;
+
+ConcreteSubject s = new ConcreteSubject();
+
+s.Attach(new ConcreteObserver(s, "X"));
+s.Attach(new ConcreteObserver(s, "Y"));
+s.Attach(new ConcreteObserver(s, "Z"));
+
+s.SubjectState = "ABC";
+s.Notify();
+
+Console.ReadLine();
